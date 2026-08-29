00bd51eee8e3b22d782f8f7d6da208e975a72206
CodyThePerson <cwright346@gatech.edu>
2026-08-29 18:12:30 -0400

    Created the homepage, books page, and individual book info pages.

--- catalog/models.py
+++ catalog/models.py
@@ -84,7 +84,7 @@ class Book(models.Model):
 
     def get_absolute_url(self):
         """Returns the URL to access a detail record for this book."""
-        return reverse('book-detail', args=[str(self.id)])
+        return reverse('book_detail', args=[str(self.id)])
 
 class BookInstance(models.Model):
 
